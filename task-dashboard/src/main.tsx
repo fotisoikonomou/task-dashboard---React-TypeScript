fbe2f14a1decb89933322f81c0fb0a8fb436eb4f
fotisoikonomou <i_fotis@hotmail.com>
2025-10-29 18:04:51 +0200

    Update main.tsx

--- task-dashboard/src/main.tsx
+++ task-dashboard/src/main.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux'; // Θα το ενεργοποιήσουμε αργότερα
+// import { Provider } from 'react-redux'; 
 import App from './App';
 import './index.css';
 
